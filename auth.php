8a0ec1a215e6179a16f3f90db9fc0034e48ba293
Claret Nnamocha <devclareo@gmail.com>
2019-09-16 23:45:48 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -32,6 +32,18 @@ function login(PDO $db_connect, string $email, string $password) {
     ];
 }
 
+
+function set_up()
+{
+    $db = connect_db('ec2-184-73-232-93.compute-1.amazonaws.com','d7o4t6ds4parku','wrhvksgoxcqzpo','75a892a211781e65baa4fdb5f64696de751a1182560962c96907bab2a0e6c5a3');
+    $tbl_sql = "CREATE TABLE `users` ( `id` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(60) NOT NULL , `password` VARCHAR(70) NOT NULL , PRIMARY KEY (`id`)) ";
+    $db->query($tbl_sql);
+    $rec_sql = "INSERT INTO `users` (`email`, `password`) VALUES ('photizo@hng.com', '$2y$10$7X0xb6ghz4yPLfTaaXazz.Hp08wsJ.DwxsQVK5734gzWjsV3sXxiO')";
+    $db->query($rec_sql);
+    print_r('ok');
+}
+
 // $db = connect_db('localhost', 'hng', 'root', '');
 // $l = login($db, 'photizo', '1234');
 // print_r($l);
+set_up();
