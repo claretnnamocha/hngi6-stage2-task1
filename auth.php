03a2c908964852f08a3d4d6516789628ea4608c3
Claret Nnamocha <devclareo@gmail.com>
2019-09-17 21:39:43 +0100

    using redbean.php

--- auth.php
+++ auth.php
@@ -1,8 +1,9 @@
 <?php
+require_once 'rb.php'
 
 function connect_db(string $host, string $dbname, string $user, string $password) {
     try {
-        return new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        return R::setup("pgsql:host=$host;dbname=$dbname", $user, $password);
     } catch (Exception $e) {
         throw new $e;
         
@@ -10,21 +11,17 @@ function connect_db(string $host, string $dbname, string $user, string $password
     }
 }
 
-function login(PDO $db_connect, string $email, string $password) {
+function login(string $email, string $password) {
     $failed = [
         'status' => false,
         'message' => 'Invalid email or password!'
     ];
-    $sql = "SELECT * FROM `users` WHERE `email` = :email";
-    $stmnt = $db_connect->prepare($sql);
-    $stmnt->execute([ 'email' => $email ]);
-    $data = $stmnt->fetchAll(PDO::FETCH_OBJ);
-    print_r($data);
-    if (!((is_array($data) and (count($data) == 1)))) {
+    $user = R::find('user','email = :email',[ 'email' => $email ]);
+    if (count($user) != 1) {
         return $failed;
     }
-    $data = array_values($data)[0];
-    if (!password_verify($password, $data->password)) {
+    $user = array_values($user)[0];
+    if (!password_verify($password, $user->password)) {
         return $failed;
     }
     return [
@@ -36,15 +33,11 @@ function login(PDO $db_connect, string $email, string $password) {
 
 function set_up()
 {
-    $db = connect_db('ec2-184-73-232-93.compute-1.amazonaws.com','d7o4t6ds4parku','wrhvksgoxcqzpo','75a892a211781e65baa4fdb5f64696de751a1182560962c96907bab2a0e6c5a3');
-    $tbl_sql = "CREATE TABLE `users` ( `id` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(60) NOT NULL , `password` VARCHAR(70) NOT NULL , PRIMARY KEY (`id`)) ";
-    $db->query($tbl_sql);
-    $rec_sql = "INSERT INTO `users` (`email`, `password`) VALUES ('photizo@hng.com', '$2y$10$7X0xb6ghz4yPLfTaaXazz.Hp08wsJ.DwxsQVK5734gzWjsV3sXxiO')";
-    $db->query($rec_sql);
-    print_r('ok');
+    $user = R::dispense('user');
+    $user->email = 'photizo@hng.com';
+    $user->password = '$2y$10$7X0xb6ghz4yPLfTaaXazz.Hp08wsJ.DwxsQVK5734gzWjsV3sXxiO';
+    R::store($user);
+    print_r('expression');
 }
 
-// $db = connect_db('localhost', 'hng', 'root', '');
-// $l = login($db, 'photizo', '1234');
-// print_r($l);
-// set_up();
+connect_db('ec2-184-73-232-93.compute-1.amazonaws.com','d7o4t6ds4parku','wrhvksgoxcqzpo','75a892a211781e65baa4fdb5f64696de751a1182560962c96907bab2a0e6c5a3');
